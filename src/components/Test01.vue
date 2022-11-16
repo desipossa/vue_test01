1ba7bae4d8e898622d958565c5cfe825d4984ffd
lee <sipossa@naver.com>
2022-11-16 17:21:21 +0900

    20221116

---
+++ src/components/Test01.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1 @click="tg" class="initial" :class="{ active: st }">
+      This is an about page {{ num }}
+    </h1>
+    <button @click="setNum">더하기</button>
+  </div>
+</template>
+  <script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const num = ref(0);
+    const setNum = () => {
+      num.value++;
+    };
+
+    const st = ref(false);
+    const tg = () => {
+      st.value = !st.value;
+    };
+    return {
+      num,
+      setNum,
+      st,
+      tg,
+    };
+  },
+};
+</script>
+  <style lang="scss" scoped>
+.about {
+  .active {
+    color: red;
+  }
+}
+</style>
+  
